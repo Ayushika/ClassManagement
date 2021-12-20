cd348a03c8288039f73d8031c94eb09215928de4
Ayushika <ayushikabansal7890@gmail.com>
2021-12-20 20:30:33 +0530

    Added App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,12 @@
+/** @format */
+
 import React from "react";
 
 const App = () => {
   return (
-    <div className="container">
-      <h1 className="text-center text-success">ClassRoom</h1>
+    <div className='container'>
+      <h1 className='text-center text-success'>ClassRoom</h1>
+      <p className='text-danger text-center'>Starts from 20-12-2021</p>
     </div>
   );
 };
