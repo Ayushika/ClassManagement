15af020e3abc51b25509fb6f5ca3b4d705b3dd4f
rghvm <rghvmunjal@gmail.com>
2021-12-21 11:26:46 +0530

    changes made

--- frontend/src/pages/HomePage.js
+++ frontend/src/pages/HomePage.js
@@ -3,7 +3,7 @@ import React from "react";
 const HomePage = () => {
   return (
     <div>
-      <h1 className="text-center text-success">ClassRoom</h1>
+      <h1 className="text-center">ClassRoom</h1>
     </div>
   );
 };
