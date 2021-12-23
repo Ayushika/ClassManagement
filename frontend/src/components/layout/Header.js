f5b5089a8ed0c4edd63bcfa0fc390967c0353c11
Ayushika <ayushikabansal7890@gmail.com>
2021-12-23 20:27:24 +0530

    Logout added

--- frontend/src/components/layout/Header.js
+++ frontend/src/components/layout/Header.js
@@ -1,27 +1,51 @@
+/** @format */
+
 import React from "react";
+import { useSelector, useDispatch } from "react-redux";
+import { useHistory } from "react-router-dom";
 import { Navbar, Nav, Container } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import { logout } from "../../actions/userAction";
 import pustak from "../../images/pustak-sm.png";
 
 const Header = () => {
+  const { userInfo } = useSelector((state) => state.userLogin);
+  const dispatch = useDispatch();
+  const history = useHistory();
+
+  /* Logout */
+  const handleLogout = () => {
+    dispatch(logout(history));
+  };
+
   return (
     <header>
-      <Navbar bg="light" expand="lg" id="header">
+      <Navbar bg='light' expand='lg' id='header'>
         <Container>
-          <Link to="/" className="custom-link">
-            <Navbar.Brand className="font-bold text-success h1">
-              <img src={pustak} alt="logo" style={{ width: "32%" }} />
+          <Link to='/' className='custom-link'>
+            <Navbar.Brand className='font-bold text-success h1'>
+              <img src={pustak} alt='logo' style={{ width: "32%" }} />
             </Navbar.Brand>
           </Link>
-          <Navbar.Toggle aria-controls="basic-navbar-nav" />
-          <Navbar.Collapse id="basic-navbar-nav">
-            <Nav className="ms-auto">
-              <Link to="/register" className=" nav-link custom-link">
-                <i className="fas fa-user-plus"></i> Register
-              </Link>
-              <Link to="/login" className=" nav-link custom-link">
-                <i className="fas fa-user"></i> Login
-              </Link>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse id='basic-navbar-nav'>
+            <Nav className='ms-auto'>
+              {userInfo && userInfo.token ? (
+                <div
+                  className=' nav-link custom-link'
+                  onClick={() => handleLogout()}>
+                  <i className='fas fa-user'></i> Logout
+                </div>
+              ) : (
+                <>
+                  <Link to='/register' className=' nav-link custom-link'>
+                    <i className='fas fa-user-plus'></i> Register
+                  </Link>
+                  <Link to='/login' className=' nav-link custom-link'>
+                    <i className='fas fa-user'></i> Login
+                  </Link>
+                </>
+              )}
             </Nav>
           </Navbar.Collapse>
         </Container>
---
+++ frontend/src/pages/admin/Dashboard.jsx
@@ -0,0 +1,9 @@
+/** @format */
+
+import React from "react";
+
+const Dashboard = () => {
+  return <h3 className='text-center text-success'>Admin</h3>;
+};
+
+export default Dashboard;
